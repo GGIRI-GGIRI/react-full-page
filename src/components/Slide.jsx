2a27f1e852d1249ea43603ea9d3b668254036061
Sejeong Hong <dev.gwisket@gmail.com>
2023-06-22 01:58:29 +0900

    use default parameters instead of defaultProps for Slide

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Slide = (props) => (
+const Slide = (props = { children: null, style: {} }) => (
   <div {...props} style={{ ...props.style, height: '100%' }}>
     {props.children}
   </div>
@@ -10,11 +10,6 @@ const Slide = (props) => (
 Slide.propTypes = {
   children: PropTypes.node,
   style: PropTypes.object,
-};
-
-Slide.defaultProps = {
-  children: null,
-  style: {},
 };
 
 Slide.isSlideOfFullpage = true;
